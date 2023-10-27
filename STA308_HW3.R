3c680e7617b159559be9fa8aae8e5cd2ac198d4d
unknown <Sara@SaraLT.DG2DESIGN.local>
2023-10-26 19:47:57 -0500

    worked on function 10/26

--- STA308_HW3.R
+++ STA308_HW3.R
@@ -2,7 +2,32 @@
 #* 
 #* Author: Grace Runge
 #* 
-#* Date
+#* Date: 10/26/2023
 #* 
-#* Inputs
+#* Purpose: Write a function called AddItUp().
+#*    The function will ask a user to input a number and they will 
+#*    continue to be prompted to enter another number until they wish 
+#*    to stop. The function will then output the numbers entered by 
+#*    the user and their sum.
+#* 
+
+AddItUp <- function(){
+  numbers_list <- list()
+  first_num <- readline(prompt = "Please enter a number:")
+  first_num_check <- as.numeric(first_num)
+  if(is.na(first_num_check)){
+    print("not number")
+  } else {
+    print("number!")
+    numbers_list <- c(numbers_list, first_num_check)
+  }
+  numbers_list
+}
+
+
+AddItUp()
+
+
+
+
 
